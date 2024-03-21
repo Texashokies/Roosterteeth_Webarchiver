b80d244d6ee2ea02b32dcc47000d4e5297908c3a
Texashokies <43481909+Texashokies@users.noreply.github.com>
2024-03-20 20:24:34 -0500

    Add more wait methods for page ready, element visibility, and element not existing.

--- src/main/java/com/roosterteeth/utility/WaitHelper.java
+++ src/main/java/com/roosterteeth/utility/WaitHelper.java
@@ -1,7 +1,10 @@
 package com.roosterteeth.utility;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -17,4 +20,29 @@ public class WaitHelper {
         wait.pollingEvery(Duration.ofMillis(250));
         wait.until((driver1 -> driver1.findElement(by)));
     }
+
+    public static void waitForElementNotExist(By by,long timeoutInSeconds, WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(timeoutInSeconds));
+        wait.pollingEvery(Duration.ofMillis(250));
+        wait.until(driver1 -> driver1.findElements(by).isEmpty());
+    }
+
+
+    public static void waitForPageReady(long timeoutInSeconds,WebDriver driver){
+        FluentWait wait = new FluentWait(driver);
+        wait.withTimeout(Duration.ofSeconds(timeoutInSeconds));
+        wait.pollingEvery(Duration.ofMillis(250));
+        wait.ignoring(NoSuchElementException.class);
+
+        wait.until( webdriver -> ((JavascriptExecutor)webdriver).executeScript("return document.readyState").equals("complete"));
+    }
+
+    public static void waitForElementVisible(By by,long timeoutInSeconds,WebDriver driver){
+        FluentWait wait = new FluentWait(driver);
+        wait.withTimeout(Duration.ofSeconds(timeoutInSeconds));
+        wait.pollingEvery(Duration.ofMillis(250));
+        wait.ignoring(NoSuchElementException.class);
+        wait.until(webDrive->driver.findElement(by).isDisplayed());
+    }
+
 }
